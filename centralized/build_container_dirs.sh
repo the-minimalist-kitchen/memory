66fe23cdb89d8949b590b7fbb34054896e757b81
taylor <brian.t.vann@gmail.com>
2024-03-25 16:34:50 -0700

    mutliple builds possible with one script via multiple .env files

--- centralized/build_container_dirs.sh
+++ centralized/build_container_dirs.sh
@@ -1,11 +1,64 @@
-script_dir=`dirname $0`
-postgres_dir=$script_dir/postgres_data
-redis_dir=$script_dir/redis_data
+####
+#
+# This script generates directories and files based on a provied .env file
+#	$ build_container_dirs.sh /path/to/.env
+#
+####
 
-mkdir $postgres_dir $redis_dir
+# LOAD ENV FILE
+case $1 in
+	*.env)
+		#mkdir $1
+		echo $1
+		source $1
+	;;
+	*)
+		#mkdir $script_dir/$1
+		echo "arg is not .env file"
+		exit 1
+	;;
+esac
 
-# if SELinux is installed
-if [ -x "$(command -v getenforce)" ]; then
-  chcon -R -t container_file_t $postgres_dir $redis_dir
-fi
+# MAKE OUTPUT RELATIVE TO .env FILE
+env_dir=`dirname $1`
+echo $env_dir
 
+# SET SELINUX CONTAINER LABELS if SELinux is installed
+set_selinux_Label () {
+	if [ -x "$(command -v getenforce)" ]; then
+		chcon -R -t container_file_t $1
+	fi
+}
+
+# ACCOUNT FOR RELATIVE PATHS IN .env FILE
+make_dirs () {
+	case $1 in
+		/*)
+			# echo $1
+			mkdir -p $1
+			set_selinux_Label $1
+		;;
+		*)
+			# echo $env_dir/$1
+			mkdir -p $env_dir/$1
+			set_selinux_Label $env_dir/$1
+		;;
+	esac
+}
+
+make_conf_labels () {
+	case $1 in
+		/*)
+			# echo $1
+			set_selinux_Label $1
+		;;
+		*)
+			# echo $env_dir/$1
+			set_selinux_Label $env_dir/$1
+		;;
+	esac
+}
+
+make_dirs $TMK3_POSTGRES_DIR
+make_dirs $TMK3_REDIS_DIR
+make_conf_labels $TMK3_REDIS_CONF
